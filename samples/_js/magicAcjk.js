27b4cd62380b6dbe501a29a8f6ab6318e766358f
parsimonhi <parsimonhi@gmail.com>
2018-11-01 10:05:22 +0100

    improve samples

--- samples/_js/magicAcjk.js
+++ samples/_js/magicAcjk.js
@@ -2,6 +2,17 @@
 // Animated_GIF project sample
 // see https://github.com/sole/Animated_GIF/blob/master/tests/basic.js
 // other parts were written from scratch
+
+// Main functions:
+//  generateRedPngFromSvg(s,size,background):
+//    s is the content of a svg file coming from MakeMeAHanzi or animCJK projects
+//    generate a red PNG image
+//    return a string which is the base64 representation of the image
+//  generateAnimatedGifFromSvg(s,size,background,delay,dec,show,save):
+//    s is the content of a svg file coming from MakeMeAHanzi or animCJK projects
+//    generate an animated gif image
+//    the image can be shown using the "show" function and saved using the "save" function
+
 function computeOne(a,k,km)
 {
 	return Math.floor((k-1)*a/Math.max(km-1,1));
@@ -12,11 +23,87 @@ function colorize(k,km)
 	var r=252,g=14,b=28;
 	return "rgb("+computeOne(r,k,km)+","+computeOne(g,k,km)+","+computeOne(b,k,km)+")";
 }
-function makeAnimatedGifFromPngs(target,delay) {
-    var imgs = document.querySelectorAll('img.ghostImg'+target.id);
+function generatePngFromSvg(paths,background,mmah,size)
+{
+	// paths is an array that contains a list of stroke d and fill attributes
+	// background will be the background of the image
+	// mmah indicates if the data come from MakeMeAHanzi instead of AnimCJK
+	// return a base64 encoded PNG image
+	var cn,cx,k,km,m,r,x0,y0,x1,y1,x2,y2,x3,y3;
+	// create a ghost canvas
+	cn=document.createElement('canvas');
+	cn.width=size;
+	cn.height=size;
+	cx=cn.getContext("2d");
+	if (size!=1024) cx.scale(size/1024,size/1024);
+	cx.fillStyle=background;
+	cx.fillRect(0,0,cn.width,cn.height);
+	// draw strokes in the canvas 
+	km=paths.length;
+	r=/ ?([MLQC]) ?([0-9-]+) ([0-9-]+) ?([0-9-]+)? ?([0-9-]+)? ?([0-9-]+)? ?([0-9-]+)?/g;
+	for(k=0;k<km;k++) // draw last strokes first
+	{
+		cx.beginPath();
+		x0=0;
+		y0=0;
+		while (m=r.exec(paths[k].d))
+		{
+			x1=parseInt(m[2]);
+			y1=parseInt(m[3]);
+			if (mmah) y1=-y1+900;
+			if (!x0) x0=x1;
+			if (!y0) y0=y1;
+			if (m[1]=="M") cx.moveTo(x1,y1);
+			else if (m[1]=="L") cx.lineTo(x1,y1);
+			else
+			{
+				x2=parseInt(m[4]);
+				y2=parseInt(m[5]);
+				if (mmah) y2=-y2+900;
+				if (m[1]=="Q") cx.quadraticCurveTo(x1,y1,x2,y2);
+				else
+				{
+					x3=parseInt(m[6]);
+					y3=parseInt(m[7]);
+					if (mmah) y3=-y3+900;
+					cx.bezierCurveTo(x1,y1,x2,y2,x3,y3);
+				}
+			}
+		}
+		cx.lineTo(x0,y0); // sometimes not necessary, but it doesn't matter
+		x0=0;
+		y0=0;
+		cx.fillStyle=paths[k].fill;
+		cx.fill();
+	}
+	// generate a base64 encoded PNG from the canvas then return it
+	return cn.toDataURL("image/png");
+}
+function generateRedPngFromSvg(s,size,background)
+{
+	// generate a "red" PNG image from a svg
+	// s is a text representing a character in svg format
+	// size will be the size of the PNG
+	// background will be the background of the PNG (including transparent)
+	var k,km,img,reg,m,paths=[],mmah;
+	// if mmah is true, assume the svg comes from MakeMeAHanzi
+	// else assume the svg comes from animCJK
+	mmah=!s.match(/class="acjk"/);
+	// extract "d" attribute values
+	if (mmah) reg=/<path d=\"([^\"]*)\" fill=\"lightgray\"/g;
+	else reg=/<path id=\"[^\"]*\" d=\"([^\"]*)\"/g;
+	while (m=reg.exec(s)) paths.push({d:m[1]});
+	km=paths.length;
+	// colorize "d"
+	for (k=0;k<km;k++) paths[k].fill=colorize(k+1,km);
+	// return base64 PNG image
+	return generatePngFromSvg(paths,background,mmah,size);
+}
+function makeAnimatedGifFromPngs(ghost,delay,background,dec,show,save) {
+    var imgs = ghost.getElementsByTagName('img');
     var firstImage = imgs[0];
-    var imageWidth = firstImage.clientWidth;
-    var imageHeight = firstImage.clientHeight;
+    var imageWidth = firstImage.getAttribute("width");
+    var imageHeight = firstImage.getAttribute("height");
     var tasks = [];
     function buildImageCallback(img) {
         return function(gif) {
@@ -29,7 +116,7 @@ function makeAnimatedGifFromPngs(target,delay) {
             var ag = new Animated_GIF({
                 repeat: null, // Don't repeat
             });
-            ag.setSize(img.clientWidth, img.clientHeight);
+            ag.setSize(img.getAttribute("width"), img.getAttribute("height"));
             ag.addFrame(img);
             var img2 = document.createElement('img');
             if(img.nextSibling) {
@@ -87,10 +174,11 @@ function makeAnimatedGifFromPngs(target,delay) {
         });
         agAll.getBase64GIF(function(image) {
             imgAll.src = image;
-            // in case of automatisation, this is the right place to save the image
+            // in case of automatisation, it's the right place to show or save the image
+            if (show) show(image, background);
+            if (save) save(image, background, dec);
             doneCallback();
         });
-        target.appendChild(imgAll);
     });
 
     for(var i = 0; i < imgs.length; i++) {
@@ -99,107 +187,30 @@ function makeAnimatedGifFromPngs(target,delay) {
 
     runTasks(tasks);
 }
-function generatePngFromSvg(paths,background,mmah)
-{
-	// paths is an array that contains a list of stroke d and fill attributes
-	// background will be the background of the image
-	// mmah indicates if the data come from MakeMeAHanzi instead of AnimCJK
-	// return a base64 encoded PNG image
-	var cn,cx,k,km,m,r,x0,y0,x1,y1,x2,y2,x3,y3;
-	// create a ghost canvas
-	cn=document.createElement('canvas');
-	cn.width=1024;
-	cn.height=1024;
-	cx=cn.getContext("2d");
-	cx.fillStyle=background;
-	cx.fillRect(0,0,cn.width,cn.height);
-	// draw strokes in the canvas 
-	km=paths.length;
-	r=/ ?([MLQC]) ?([0-9-]+) ([0-9-]+) ?([0-9-]+)? ?([0-9-]+)? ?([0-9-]+)? ?([0-9-]+)?/g;
-	for(k=0;k<km;k++) // draw last strokes first
-	{
-		cx.beginPath();
-		x0=0;
-		y0=0;
-		while (m=r.exec(paths[k].d))
-		{
-			x1=parseInt(m[2]);
-			y1=parseInt(m[3]);
-			if (mmah) y1=-y1+900;
-			if (!x0) x0=x1;
-			if (!y0) y0=y1;
-			if (m[1]=="M") cx.moveTo(x1,y1);
-			else if (m[1]=="L") cx.lineTo(x1,y1);
-			else
-			{
-				x2=parseInt(m[4]);
-				y2=parseInt(m[5]);
-				if (mmah) y2=-y2+900;
-				if (m[1]=="Q") cx.quadraticCurveTo(x1,y1,x2,y2);
-				else
-				{
-					x3=parseInt(m[6]);
-					y3=parseInt(m[7]);
-					if (mmah) y3=-y3+900;
-					cx.bezierCurveTo(x1,y1,x2,y2,x3,y3);
-				}
-			}
-		}
-		cx.lineTo(x0,y0); // sometimes not necessary, but it doesn't matter
-		x0=0;
-		y0=0;
-		cx.fillStyle=paths[k].fill;
-		cx.fill();
-	}
-	// generate a base64 encoded PNG from the canvas then return it
-	return cn.toDataURL("image/png");
-}
-function generateRedPngFromSvg(s,target,background)
-{
-	// generate a "red" PNG image from a svg
-	// s is a text representing a character in svg format
-	// target is a HTML element where the image will be displayed
-	// background will be the background of the image (including transparent)
-	// the size of the image will be the size of target
-	var k,km,img,reg,m,paths=[],mmah;
-	// if mmah is true, assume the svg comes from MakeMeAHanzi
-	// else assume the svg comes from animCJK
-	mmah=!s.match(/class="acjk"/);
-	// extract "d" attribute values
-	if (mmah) reg=/<path d=\"([^\"]*)\" fill=\"lightgray\"/g;
-	else reg=/<path id=\"[^\"]*\" d=\"([^\"]*)\"/g;
-	while (m=reg.exec(s)) paths.push({d:m[1]});
-	km=paths.length;
-	// colorize "d"
-	for (k=0;k<km;k++) paths[k].fill=colorize(k+1,km);
-	// create and display a PNG image in target
-	img=document.createElement("img");
-	img.style.display="block";
-	img.style.border="0";
-	img.width=target.clientWidth;
-	img.height=target.clientHeight;
-	img.src=generatePngFromSvg(paths,background,mmah);
-	// in case of automatisation, this is the right place to save the image
-	target.appendChild(img);
-}
-function generateAnimatedGifFromSvg(s,target,delay,background)
+function generateAnimatedGifFromSvg(s,size,background,delay,dec,show,save)
 {
 	// generate an animated GIF image from a svg
 	// s is a text representing a character in svg format
-	// target is a HTML element where the image will be displayed
+	// size will be the size of the PNG
 	// background will be the background of the image (excepting transparent)
 	// if background is transparent, make grey strokes transparent and background white
 	// delay is the delay between two frames
-	// the size of the animated GIF image will be the size of target
+	// dec is the decimal unicode of the character
+	// show is a function called to show the image somewhere at the end of the process
+	// show has 2 parameters:
+	//  image which is a base64 representation of the image
+	//  background which can be "transparent" or any css color
+	// save is a function called to save the image somewhere at the end of the process
+	// save has 3 parameters:
+	//  image which is a base64 representation of the image
+	//  background which can be "transparent" or any css color
+	//  dec which is the decimal unicode of the character
 	var k1,k2,km,img,imgsSrc=[],ghost,mmah,m,paths,reg;
 	// if mmah is true, assume the svg comes from MakeMeAHanzi
 	// else assume the svg comes from animCJK
-	mmah=!s.match(/class="acjk"/);
+	mmah=!s.match(/class="acjk/);
 	// create a ghost div that will contains ephemeral PNG images
 	ghost=document.createElement("div");
-	ghost.style.opacity="0"; // avoid disturbing page display
-	ghost.style.position="absolute"; // avoid disturbing page display
-	target.parentNode.appendChild(ghost);
 	// extract "d" attributes
 	if (mmah) reg=/<path d=\"([^\"]*)\" fill=\"lightgray\"/g;
 	else reg=/<path id=\"[^\"]*\" d=\"([^\"]*)\"/g;
@@ -226,38 +237,29 @@ function generateAnimatedGifFromSvg(s,target,delay,background)
 			// second trick to manage animated image with transparent background
 			// if background parameter is "transparent", draw white background
 			// else draw background as is
+			// size is always 1024 here even if the generated image has a different size
 			if (background=="transparent")
-				imgsSrc[k1]=generatePngFromSvg(paths,"#fff",mmah);
-			else imgsSrc[k1]=generatePngFromSvg(paths,background,mmah);
+				imgsSrc[k1]=generatePngFromSvg(paths,"#fff",mmah,1024);
+			else imgsSrc[k1]=generatePngFromSvg(paths,background,mmah,1024);
 		}
 		for (k1=km;k1>=0;k1--)
 		{
 			img=document.createElement('img');
 			img.id="img"+k1;
-			img.className="ghostImg"+target.id;
 			img.style.display="block";
 			img.style.border="0";
-			img.width=target.clientWidth;
-			img.height=target.clientHeight;
+			img.width=size;
+			img.height=size;
 			img.acjk={};
 			img.acjk.km=km;
-			img.acjk.target=target;
-			img.acjk.delay=delay;
-			img.acjk.ghost=ghost;
 			img.onload=function(){
-				var k,km,target,delay,allDone=true;
+				var k,km,allDone=true;
 				this.done=1;
 				km=this.acjk.km;
-				target=this.acjk.target;
-				delay=this.acjk.delay;
-				ghost=this.acjk.ghost;
 				for(k=0;k<=km;k++)
-					if (!document.getElementById("img"+k).done) {allDone=false;break;}
+					if (!ghost.querySelector("#img"+k).done) {allDone=false;break;}
 				if (allDone) // ready to generate GIF image
-				{
-					makeAnimatedGifFromPngs(target,delay);
-					target.parentNode.removeChild(ghost); // remove ghost div and its imgs
-				}
+					makeAnimatedGifFromPngs(ghost,delay,background,dec,show,save);
 			};
 			img.src=imgsSrc[k1];
 			ghost.appendChild(img);
