9fd097d53f63ca1b7635485c91f407cfa6e87fbc
parsimonhi <parsimonhi@gmail.com>
2025-09-30 18:09:14 +0200

    various updates

--- lib.php
+++ lib.php
@@ -1,7 +1,6 @@
 <?php
 include_once __DIR__."/samples/_php/encoding.php";
 include_once __DIR__."/samples/_php/unicode.php";
-include_once __DIR__."/samples/_php/convertKana.php";
 
 if( !function_exists('mb_str_split')){
 	// some old php do not have it
